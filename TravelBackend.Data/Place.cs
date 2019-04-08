c5255453caf1c3610bc1974a7eca7c9651ff28ea
JohnChiapetto <chiapettoj@gmail.com>
2019-04-08 11:26:14 -0400

    Added classes for all place models. Need to update these still.

---
+++ TravelBackend.Data/Place.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelBackend.Data
+{
+    public class Place
+    {
+        [Key]
+        public Guid PlaceId { get; set; }
+
+        [Required]
+        public string PlaceName { get; set; }
+
+        [Required]
+        public string PlaceLocation { get; set; }
+
+        [Required]
+        public string PlaceDescription { get; set; }
+
+        public string PlaceImageUrl { get; set; } = null;
+
+        [Required]
+        public DateTimeOffset SubmittedUTC { get; set; }
+        public DateTimeOffset? ModifiedUTC { get; set; } = null;
+
+        [Required]
+        public Guid SubmittingUserId { get; set; }
+        public Guid? ModifyingUserId { get; set; } = null;
+
+        public Place()
+        {
+            PlaceId = Guid.NewGuid();
+        }
+    }
+}
