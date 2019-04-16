a2efbbcafef48b3ab27104050a602069872b91f6
JohnChiapetto <chiapettoj@gmail.com>
2019-04-16 11:50:20 -0400

    Added AbstractController. Certain actions now require authentication, and some now require Administrator priviledges.

---
+++ TravelBackend.Web/Controllers/AbstractController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TravelBackend.Services;
+
+namespace TravelBackend.Web.Controllers
+{
+    public abstract class AbstractController : ApiController
+    {
+        protected Guid GetUserId() => User.Identity.IsAuthenticated ? Guid.Parse(User.Identity.GetUserId()) : new Guid() ;
+        protected bool IsUserAdmin
+        {
+            get
+            {
+                var svc = new RoleService(GetUserId());
+                var rid = Guid.Parse(svc.GetRole(e => e.Name == "Admin").Id);
+                return svc.IsUserInRole(GetUserId(),rid);
+            }
+        }
+    }
+}
